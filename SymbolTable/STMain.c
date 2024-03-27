889389048b705635b254784d48ea1c4c8c943368
sforseohn <sforseohn@ewhain.net>
2024-03-27 16:55:10 +0900

    Feat: add examples

--- SymbolTable/STMain.c
+++ SymbolTable/STMain.c
@@ -98,73 +98,74 @@ int main() {
 
 	init_sym_table();
 
-	result = fopen_s(&fp, "example2_error.txt", "r"); // 파일을 읽기 모드로 열기
+	result = fopen_s(&fp, "example2.txt", "r"); // 파일을 읽기 모드로 열기
 	if (result != 0) {
 		printf("파일 열기 실패(%d)\n", result);
 		return -1;
 	}
-	 
+
 	while ((c = fgetc(fp)) != EOF) { // 파일 끝까지 문자 읽기
+		if (strchr(separators, c) == NULL) {
+			str_pool[index_next++] = (char)c; // 버퍼에 문자 저장
+			continue;
+		}
+
 		// 구분자를 만나거나 버퍼 크기 제한에 도달했을 때
-		if (strchr(separators, c) != NULL) {
-			if (index_start < index_next) { // 버퍼에 내용이 있을 때만 출력
+		if (index_start < index_next) { // 버퍼에 내용이 있을 때만 출력
 
-				// 입력이 string pool의 크기를 초과할 경우
-				// 마지막 문자열은 제외하도록 처리하고 while문을 빠져나감.
-				if (index_next >= SYM_TABLE_SIZE) {
-					index_next = index_start; // 버퍼 인덱스 초기화
-					printf("Error - OVERFLOW...\n\n");
-					break;
-				}
+			// 입력이 string pool의 크기를 초과할 경우
+			// 마지막 문자열은 제외하도록 처리하고 while문을 빠져나감.
+			if (index_next >= SYM_TABLE_SIZE) {
+				index_next = index_start; // 버퍼 인덱스 초기화
+				printf("Error - OVERFLOW...\n\n");
+				break;
+			}
 
-				str_pool[index_next] = '\0'; // 문자열 종료
-				// 문자열의 시작 문자가 숫자인지 체크
-				if (str_pool[index_start] >= '0' && str_pool[index_start] <= '9') {
-					printf("Error - start with digit (%s)\n", str_pool + index_start); // 에러 출력
-					index_next = index_start; // 버퍼 인덱스 초기화
+			str_pool[index_next] = '\0'; // 문자열 종료
+			// 문자열의 시작 문자가 숫자인지 체크
+			if (str_pool[index_start] >= '0' && str_pool[index_start] <= '9') {
+				printf("Error - start with digit (%s)\n", str_pool + index_start); // 에러 출력
+				index_next = index_start; // 버퍼 인덱스 초기화
+			}
+			// 식별자의 길이가 15자 이내인지 체크
+			else if (index_next - index_start > MAX_IDENTIFIER_SIZE) {
+				printf("Error - maximum length is 15 (%s)\n", str_pool + index_start);
+				index_next = index_start;  // 버퍼 인덱스 초기화
+			}
+			else {
+				// 영어 대소문자, 밑줄, 숫자, 구분자 이외의 문자가 있는지 체크
+				int is_valid = 1;
+				for (int i = index_start; i <= index_next; i++) {
+					if (!isalnum(str_pool[i]) && str_pool[i] != '_' && !strchr(separators, str_pool[i])) { // isalnum: 알파벳이나 숫자면 1을 반환
+						is_valid = 0;
+					}
 				}
-				// 식별자의 길이가 15자 이내인지 체크
-				else if (index_next - index_start > MAX_IDENTIFIER_SIZE) {
-					printf("Error - maximum length is 15 (%s)\n", str_pool + index_start);
-					index_next = index_start;  // 버퍼 인덱스 초기화
+				if (!is_valid) {
+					printf("Error - invalid character in identifier (%s)\n", str_pool + index_start);
+					index_next = index_start; // 버퍼 인덱스 초기화
 				}
 				else {
-					// 영어 대소문자, 밑줄, 숫자, 구분자 이외의 문자가 있는지 체크
-					int is_valid = 1;
-					for (int i = index_start; i <= index_next; i++) {
-						if (!isalnum(str_pool[i]) && str_pool[i] != '_' && !strchr(separators, str_pool[i])) { // isalnum: 알파벳이나 숫자면 1을 반환
-							is_valid = 0;
-						}
-					}
-					if (!is_valid) {
-						printf("Error - invalid character in identifier (%s)\n", str_pool + index_start);
-						index_next = index_start; // 버퍼 인덱스 초기화
+					sym_table[index][0] = index_start; // 현재 처리 중인 문자열의 시작 인덱스를 심볼 테이블에 기록
+					sym_table[index++][1] = (int)strlen(str_pool + index_start); // str_pool의 index_start 위치부터 문자열의 끝까지 길이 계산
+
+					hash_value = divisionMethod(str_pool + index_start, HASH_TABLE_SIZE);
+
+					HTpointer htp = lookup_hash_table(index_start, hash_value);
+					if (htp == NULL) {
+						add_hash_table(index_start, hash_value);
+						printf("%s (Hash: %d)\n", str_pool + index_start, hash_value); // 버퍼 내용 화면에 출력
 					}
 					else {
-						sym_table[index][0] = index_start; // 현재 처리 중인 문자열의 시작 인덱스를 심볼 테이블에 기록
-						sym_table[index++][1] = (int)strlen(str_pool + index_start); // str_pool의 index_start 위치부터 문자열의 끝까지 길이 계산
-
-						hash_value = divisionMethod(str_pool + index_start, HASH_TABLE_SIZE);
-
-						HTpointer htp = lookup_hash_table(index_start, hash_value);
-						if (htp == NULL) {
-							add_hash_table(index_start, hash_value);
-							printf("%s (Hash: %d)\n", str_pool + index_start, hash_value); // 버퍼 내용 화면에 출력
-						}
-						else {
-							printf("%s (Already exists, Hash: %d)\n", str_pool + index_start, hash_value); // 버퍼 내용 화면에 출력
-							index_next = index_start; // 버퍼 인덱스 초기화
-						}
+						printf("%s (Already exists, Hash: %d)\n", str_pool + index_start, hash_value); // 버퍼 내용 화면에 출력
+						index_next = index_start; // 버퍼 인덱스 초기화
 					}
-					index_start = ++index_next; // 다음 문자열의 시작 인덱스 설정
 				}
+				index_start = ++index_next; // 다음 문자열의 시작 인덱스 설정
 			}
-			if (strchr(separators, c) == NULL) {
-				str_pool[index_next++] = (char)c; // 구분자가 아닌 문자를 버퍼에 저장
-			}
-			continue; // 다음 문자로 이동
 		}
-		str_pool[index_next++] = (char)c; // 버퍼에 문자 저장
+		if (strchr(separators, c) == NULL) {
+			str_pool[index_next++] = (char)c; // 구분자가 아닌 문자를 버퍼에 저장
+		}
 	}
 
 	if (index_start < index_next) { // 마지막 문자열 출력
